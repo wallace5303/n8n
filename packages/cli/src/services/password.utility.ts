b4fa6fa1deba2e054bda5c7189e0817c6b27540e
gaoshuaixing <gaoshuaixing@bilibili.com>
2026-04-27 10:38:40 +0800

    pwd 还原

--- packages/cli/src/services/password.utility.ts
+++ packages/cli/src/services/password.utility.ts
@@ -6,15 +6,15 @@ const SALT_ROUNDS = 10;
 @Utility()
 export class PasswordUtility {
 	async hash(plaintext: string) {
-		return plaintext;
-		//return await hash(plaintext, SALT_ROUNDS);
+		//return plaintext;
+		return await hash(plaintext, SALT_ROUNDS);
 	}
 
 	async compare(plaintext: string, hashed: string | null) {
 		if (hashed === null) {
 			return false;
 		}
-		return plaintext === hashed;
-		//return await compare(plaintext, hashed);
+		//return plaintext === hashed;
+		return await compare(plaintext, hashed);
 	}
 }
