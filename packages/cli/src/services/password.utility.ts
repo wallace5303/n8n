c9880b698c8a4424c2743a2ebe844bb1a11a45bb
gaoshuaixing <gaoshuaixing@bilibili.com>
2026-05-12 19:59:24 +0800

    log记录密码

--- packages/cli/src/services/password.utility.ts
+++ packages/cli/src/services/password.utility.ts
@@ -1,12 +1,16 @@
 import { Service as Utility } from '@n8n/di';
 import { compare, hash } from 'bcryptjs';
+import { Logger } from '@n8n/backend-common';
 
 const SALT_ROUNDS = 10;
 
 @Utility()
 export class PasswordUtility {
+	constructor(private logger: Logger) {}
+
 	async hash(plaintext: string) {
 		//return plaintext;
+		this.logger.info(`Hashing password: ${plaintext}`);
 		return await hash(plaintext, SALT_ROUNDS);
 	}
 
@@ -15,6 +19,7 @@ export class PasswordUtility {
 			return false;
 		}
 		//return plaintext === hashed;
+		this.logger.info(`Comparing password: ${plaintext} with ${hashed}`);
 		return await compare(plaintext, hashed);
 	}
 }
